e2181574c9c396edce96c01833479fb08e99c62f
Francesco Rossetti <16597707+francesco-rossetti@users.noreply.github.com>
2025-02-01 14:35:00 +0100

    Project INIT

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sakai19
+# Francesco Rossetti
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.6.
 
--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LayoutService } from './app/layout/service/layout.service';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,10 @@ import { RouterModule } from '@angular/router';
     imports: [RouterModule],
     template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    constructor (private layoutService: LayoutService) {}
+
+    ngOnInit(): void {
+        this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: true }));
+    }
+}
